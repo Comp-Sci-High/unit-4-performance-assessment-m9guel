ac784dc153641a06b569738af04feda43c48c71c
m9guel <miguel.sanchez26@compscihigh.org>
2026-03-05 00:57:01 +0000

    Done

--- index.js
+++ index.js
@@ -26,22 +26,63 @@ app.use((req, res, next) => {
 
 // 5) Paste in your data array of objects from the planning documents here. 
 
-
+const posts = [
+  {
+    username: "miguel:",
+    profileImage: "images/profile1.jpg",
+    imageUrl: "images/post1.jpg",
+    caption: "zzz",
+    likes: 87
+  },
+  {
+    username: "hennedys:",
+    profileImage: "images/profile2.jpg",
+    imageUrl: "images/post2.jpg",
+    caption: "light money",
+    likes: 67
+  },
+  {
+    username: "michaelr:",
+    profileImage: "images/profile3.jpg",
+    imageUrl: "images/post3.jpg",
+    caption: "better than the rest",
+    likes: 132
+  },
+  {
+    username: "JaylenG:",
+    profileImage: "images/profile4.jpg",
+    imageUrl: "images/post4.jpg",
+    caption: "put yo money up not yo funny up",
+    likes: 23
+  },
+  {
+    username: "Leandro:",
+    profileImage: "images/profile5.jpg",
+    imageUrl: "images/post5.jpg",
+    caption: "mine > miguels",
+    likes: 1523
+  }
+];
 // 6) Add static file middleware here
-
+app.use(express.static(__dirname + '/public'))
 
 // 7) Set the view engine to ejs here
-
+app.set('view engine', 'ejs')
 
 // 8) Create a route handler to path / and send your index.html 
 // run your server with node index.js to test it
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 
 // 9) Create a route handler to your overview route from your planning document
 // Pass in your data array
 
-
+app.get('/overview', (req, res) => {
+    res.render('overview', { posts: posts })
+})
 
 
 // 10) Go to your EJS file and turn the HTML into EJS
